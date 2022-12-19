ac4a570f6b80aceade9b91f9d935bf5cec7d928d
caseybetts <casey.betts@colorado.edu>
2022-12-19 16:24:05 -0700

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+console.log("JS is working");
+
+// const about_label = document.getElementById("About");
+// const about_dropdown = document.getElementById("About_Dropdown");
+// const aboutElems = document.querySelectorAll(".about");
+// var inAbout = false;
+// var inResources = false;
+
+// aboutElems.addEventListener("click", function () {
+//   console.log("click hamburger");
+//   //Open About Menu
+//   aboutElems.forEach(function (element) {
+//     element.classList.add("unhide");
+//     element.classList.remove("hide");
+//   });
+// });
+
+// about_dropdown.addEventListener("mouseover", function () {
+//   console.log("On Dropdown");
+//   inAbout = true;
+// });
+
+// about_dropdown.addEventListener("mouseout", function () {
+//   console.log("out of about");
+//   fadeElems.forEach(function (element) {
+//     element.classList.remove("fade-in");
+//     element.classList.add("fade-out");
+//   });
+// });
