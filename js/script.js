19c6562f5e80de39fec05ed024760169af7b9713
caseybetts <casey.betts@colorado.edu>
2023-01-20 14:52:20 -0700

    Added JS for text-size responsive to scroll

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
+@supports (grid-area: auto) {
+    @media screen and (min-width: 1024px) {
+
 console.log("JS is working");
+
+var cont = document.getElementById("content");
+
+window.addEventListener("scroll", function () {
+  const current = window.scrollY;
+  console.log(current);
+
+  if (current < 100) {
+    cont.style.fontSize = "30px";
+  } else if (current > 800) {
+    cont.style.fontSize = "45px";
+  } else {
+    cont.style.fontSize = 27 + current / 46 + "px";
+  }
+});
+
+    }
+}
+
+
 
 // const about_label = document.getElementById("About");
 // const about_dropdown = document.getElementById("About_Dropdown");
